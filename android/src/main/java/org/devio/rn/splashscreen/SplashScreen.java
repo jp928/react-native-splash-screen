2e1d843418c065ee3762f38a406962cbd3556e99
jp928 <piaojingtai@gmail.com>
2021-10-26 16:36:23 +1100

    feat: add video support

--- android/src/main/java/org/devio/rn/splashscreen/SplashScreen.java
+++ android/src/main/java/org/devio/rn/splashscreen/SplashScreen.java
@@ -1,8 +1,15 @@
 package org.devio.rn.splashscreen;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.Dialog;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Build;
+import android.os.SystemClock;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.VideoView;
 
 import java.lang.ref.WeakReference;
 
@@ -17,6 +24,8 @@ import java.lang.ref.WeakReference;
 public class SplashScreen {
     private static Dialog mSplashDialog;
     private static WeakReference<Activity> mActivity;
+    @SuppressLint("StaticFieldLeak")
+    private static VideoView mVideoView;
 
     /**
      * 打开启动屏
@@ -32,9 +41,55 @@ public class SplashScreen {
                     mSplashDialog.setContentView(R.layout.launch_screen);
                     mSplashDialog.setCancelable(false);
 
+                    Uri uri = Uri.parse("android.resource://"+activity.getPackageName()+"/"+ R.raw.splash); //Declare your url here.
+
+                    mVideoView = mSplashDialog.findViewById(R.id.video_view);
+                    mVideoView.setVideoURI(uri);
+
+                    mVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                        @Override
+                        public void onPrepared(MediaPlayer mp) {
+                            //Get your video's width and height
+                            int videoWidth = mp.getVideoWidth();
+                            int videoHeight = mp.getVideoHeight();
+
+                            int videoViewWidth = mVideoView.getWidth();
+                            int videoViewHeight = mVideoView.getHeight();
+
+                            float xScale = (float) videoViewWidth / videoWidth;
+                            float yScale = (float) videoViewHeight / videoHeight;
+
+                            //For Center Inside use the Math.min scale.
+                            //I prefer Center Inside so I am using Math.min
+                            float scale = Math.min(xScale, yScale);
+                            mVideoView.setScaleX(1/scale);
+                            mVideoView.setScaleY(1/scale);
+
+                            float scaledWidth = scale * videoWidth;
+                            float scaledHeight = scale * videoHeight;
+
+                            // Set the new size for the VideoView based on the dimensions of the video
+                            ViewGroup.LayoutParams layoutParams = mVideoView.getLayoutParams();
+                            layoutParams.width = (int)scaledWidth;
+                            layoutParams.height = (int)scaledHeight;
+                            mVideoView.setLayoutParams(layoutParams);
+                        }
+                    });
+
+                    mVideoView.requestFocus();
+                    mVideoView.seekTo(1);
+
                     if (!mSplashDialog.isShowing()) {
                         mSplashDialog.show();
                     }
+
+                   mSplashDialog.getWindow().getDecorView().setSystemUiVisibility(
+                           View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                                       | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                                       | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                                       | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                                       | View.SYSTEM_UI_FLAG_FULLSCREEN
+                                       | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
                 }
             }
         });
@@ -53,7 +108,7 @@ public class SplashScreen {
      * 打开启动屏
      */
     public static void show(final Activity activity) {
-        show(activity, false);
+        show(activity, true);
     }
 
     /**
@@ -75,6 +130,9 @@ public class SplashScreen {
             @Override
             public void run() {
                 if (mSplashDialog != null && mSplashDialog.isShowing()) {
+                    mVideoView.start();
+
+                    SystemClock.sleep(2200);
                     boolean isDestroyed = false;
 
                     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
